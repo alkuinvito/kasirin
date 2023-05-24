290ab0837aff9f61123f24725ce7c932819680b7
alkuinvito <alkuinvito@tutanota.com>
2023-05-24 17:55:33 +0700

    update: remove SessionProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
-import { SessionProvider } from "next-auth/react";
 import "./globals.css";
 import Header from "@/components/layout/header";
 import { Inter } from "next/font/google";
 import { Suspense } from "react";
-import { Session } from "next-auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,21 +13,17 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-  session,
 }: {
   children: React.ReactNode;
-  session: Session;
 }) {
   return (
-    <SessionProvider session={session}>
-      <html lang="en">
-        <body className={inter.className}>
-          <Suspense fallback="...">
-            <Header />
-          </Suspense>
-          <main className="mt-14">{children}</main>
-        </body>
-      </html>
-    </SessionProvider>
+    <html lang="en">
+      <body className={inter.className}>
+        <Suspense fallback="...">
+          <Header />
+        </Suspense>
+        <main className="mt-14">{children}</main>
+      </body>
+    </html>
   );
 }
