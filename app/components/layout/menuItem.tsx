678051e69ce9138548357f21cbfb7df28392c8b0
antoniuswisnu14 <antoniuswisnu50@gmail.com>
2023-05-30 17:37:05 +0700

    add: order layout

--- app/components/layout/menuItem.tsx
+++ app/components/layout/menuItem.tsx
@@ -11,12 +11,17 @@ export default function MenuItem({
   price: number
 }) {
   return (
+    <div className="">
     <div className="flex flex-col items-center bg-gray-300 rounded-lg">
       <Image className="w-full rounded-t-lg" src={image} alt={name} width={100} height={100}></Image>
       <span>{name}</span>
       <span>{price}</span>
       <div className="text-center"><button>-</button><span>4</span><button>+</button>
       </div>
+    </div>
+    <div className="">
+
+    </div>
     </div>
   );
 }
