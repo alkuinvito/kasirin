a051f3d125c381b1ee0ebce0ba77467f28440398
alkuinvito <alkuinvito@tutanota.com>
2023-06-02 14:50:38 +0700

    fix: admin middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,21 @@
+import { NextResponse } from "next/server";
 import { withAuth } from "next-auth/middleware";
 
-export default withAuth({
-  pages: {
-    signIn: "/auth/signin",
-    error: "/auth/error",
+export default withAuth(
+  function middleware(req) {
+    if (req.nextUrl.pathname.startsWith("/admin")) {
+      console.log("You are logged in as", req.nextauth.token?.role);
+      if (req.nextauth.token?.role !== "admin") {
+        return NextResponse.rewrite(
+          new URL("/auth/signin?error=AccessDenied", req.url)
+        );
+      }
+    }
   },
-});
+  {
+    pages: {
+      signIn: "/auth/signin",
+      error: "/auth/error",
+    },
+  }
+);
