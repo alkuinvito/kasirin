40b2a325b1d37b381c0a18323d51ab9281098df7
alkuinvito <alkuinvito@tutanota.com>
2023-06-15 12:49:53 +0700

    fix: add missing "key" prop

--- components/shared/fieldErrors.tsx
+++ components/shared/fieldErrors.tsx
@@ -2,8 +2,8 @@ export default function FieldErrors(props: { errors?: string[] }) {
   if (!props.errors?.length) return null;
   return (
     <div>
-      {props.errors.map((err) => (
-        <p className="text-red-600 text-sm">{err}</p>
+      {props.errors.map((err, index) => (
+        <p key={index} className="text-red-600 text-sm">{err}</p>
       ))}
     </div>
   );
