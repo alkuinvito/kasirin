0a971df0d8bb1fc41f326e7859b10248a302acf6
antoniuswisnu14 <antoniuswisnu50@gmail.com>
2023-06-16 17:12:29 +0700

    feat(signin):update signin page style

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -10,17 +10,27 @@ export default function SignIn({
   providers,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <div className="max-w-md p-4 mx-auto rounded-lg bg-slate-800">
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button
-            onClick={() => signIn(provider.id)}
-            className="py-2 px-3 w-full box-border rounded-md text-center bg-blue-700 hover:bg-blue-800 font-semibold cursor-pointer"
-          >
-            Sign in with {provider.name}
-          </button>
+    <div className="flex h-96 items-center">
+      <div className="bg-white shadow-2xl w-72 h-72 rounded-lg mx-auto flex flex-col dark:bg-slate-900">
+        <h1 className="bg-clip-text text-transparent bg-gradient-to-tr from-indigo-700 to-pink-400 font-bold text-3xl text-center pt-9">
+          KasirIn
+        </h1>
+        <p className="text-center p-3">
+          Make your business easier with Kasirin
+        </p>
+        <div className="p-4 rounded-lg text-center mt-10">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                onClick={() => signIn(provider.id)}
+                className="py-2 px-5 w-fit box-border rounded-md text-center text-white bg-blue-600 hover:bg-blue-800 font-semibold cursor-pointer shadow-lg"
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 }
