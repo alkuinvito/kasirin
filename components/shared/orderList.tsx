5cd163a9e5e164325b9c3e77b424971c3b02b8d5
alkuinvito <alkuinvito@tutanota.com>
2023-06-19 20:34:45 +0700

    feat(menu): remove categories filter

--- components/shared/orderList.tsx
+++ components/shared/orderList.tsx
@@ -1,6 +1,6 @@
 export default function OrderList() {
   return (
-    <div className="w-96 h-80 ml-5 p-4 sticky top-20 bg-gray-300 dark:bg-slate-900 rounded-lg hidden lg:block">
+    <div className="w-96 h-80 p-4 bg-gray-300 dark:bg-slate-900 rounded-lg hidden lg:block">
       <h1 className="text-2xl font-bold">Order</h1>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,64 +6,58 @@ import { menuSchema } from "@/lib/schema";
 import axios from "axios";
 import { GetServerSidePropsContext } from "next";
 import type { MenuProps } from "@/lib/schema";
+import { toSnakeCase } from "@/lib/helper";
 
 export default function Home({ products, query }: MenuProps) {
-  const [category, setCategory] = useState(products?.categories[0].name);
-  const [filtered, setFiltered] = useState(products?.categories[0]);
-
-  const handleChange = (category: string) => {
-    setCategory(category);
-    const newFiltered = products.categories.find((c) => c.name === category);
-    if (newFiltered !== undefined) {
-      setFiltered(newFiltered);
-    }
-  };
+  const [filtered, setFiltered] = useState(products?.categories);
 
   useEffect(() => {
     if (query != "") {
       axios
         .get(`/api/products?q=${query}`, { withCredentials: true })
         .then((res) => {
-          setCategory(res.data.categories[0].name);
-          setFiltered(res.data.categories[0]);
+          setFiltered(res.data.categories);
         });
     } else {
-      handleChange(category);
+      setFiltered(products.categories);
     }
   }, [query]);
 
   return (
     <div className="flex">
       <section className="grow">
-        <div className="flex gap-2">
-          {products?.categories.map((c) => (
-            <button
-              key={c.id}
-              onClick={() => handleChange(c.name)}
-              className={
-                c.name === category
-                  ? "border border-solid px-2 py-1 rounded-lg cursor-pointer hover:bg-indigo-900 dark:hover:bg-indigo-800 text-white bg-indigo-700 border-indigo-700"
-                  : "border border-solid px-2 py-1 rounded-lg cursor-pointer hover:bg-indigo-300 dark:hover:bg-indigo-950 text-indigo-700"
-              }
-            >
-              {c.name}
-            </button>
-          ))}
-        </div>
         <ul>
-          {
-            <li key={filtered.id} className="mb-4">
-              <h1 className="my-2 text-xl font-semibold">{filtered.name}</h1>
+          {filtered.map((category) => (
+            <li
+              id={toSnakeCase(category.name)}
+              key={category.id}
+              className="mb-4"
+            >
+              <h1 className="my-2 text-xl font-semibold">{category.name}</h1>
               <div className={styles.MenuList}>
-                {filtered.products.map((item) => (
-                  <MenuItem key={item.id} product={item}></MenuItem>
+                {category.products.map((item) => (
+                  <>
+                    <MenuItem key={item.id} product={item}></MenuItem>
+                  </>
                 ))}
               </div>
             </li>
-          }
+          ))}
         </ul>
       </section>
-      <OrderList></OrderList>
+      <section className="h-fit sticky top-20 ml-5">
+        <div>
+          {products.categories.map((category) => (
+            <a
+              className="rounded-lg px-2 py-1 mr-4 border-solid border border-indigo-700 text-indigo-700 hover:bg-indigo-700 hover:text-white transition-colors "
+              href={`#${toSnakeCase(category.name)}`}
+            >
+              {category.name}
+            </a>
+          ))}
+        </div>
+        <OrderList></OrderList>
+      </section>
     </div>
   );
 }
@@ -78,7 +72,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   };
 
   const response = await axios.request(options);
-
   const products = menuSchema.safeParse(response.data);
 
   if (products.success) {
