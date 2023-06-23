c82383ce4d0f83cc02228dd1723c9deada77f67d
alkuinvito <alkuinvito@tutanota.com>
2023-06-23 22:16:46 +0700

    feat: rebuild user management API

---
+++ pages/api/admin/users/[id].ts
@@ -0,0 +1,117 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Prisma } from "@prisma/client";
+import { prisma } from "@/lib/db";
+import { Role, UserModelSchema } from "@/lib/schema";
+import { getToken } from "next-auth/jwt";
+import z from "zod";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { id } = req.query;
+  const query = z.string().cuid().safeParse(id);
+  if (!query.success) {
+    return res.status(400).json({
+      error: "Invalid user id",
+    });
+  }
+
+  const token = await getToken({ req });
+  if (!token) {
+    return res.status(401).json({
+      error: "You must logged in to access/update profile",
+    });
+  }
+
+  switch (req.method) {
+    case "GET":
+      const user = await prisma.user.findUnique({
+        where: {
+          id: query.data,
+        },
+      });
+      const response = UserModelSchema.safeParse(user);
+      if (response.success)
+        return res.status(200).json({ user: response.data });
+      return res.status(500).json({ error: "Failed to retrive user" });
+    case "PATCH":
+      if (token.role === Role.enum.owner) {
+        const info = UserModelSchema.safeParse(req.body);
+        if (info.success) {
+          try {
+            const updated = await prisma.user.update({
+              where: {
+                id: query.data,
+              },
+              data: {
+                name: info.data.name,
+                image: info.data.image,
+                phone: info.data.phone,
+                address: info.data.address,
+                gender: info.data.gender,
+                dob: info.data.dob,
+                role: info.data.role,
+              },
+            });
+            const response = UserModelSchema.parse(updated);
+            return res.status(200).json({ user: response });
+          } catch (e) {
+            if (e instanceof Prisma.PrismaClientKnownRequestError) {
+              if (e.code === "P2003") {
+                return res.status(404).json({
+                  error: "User with this id is not exist",
+                });
+              }
+            }
+
+            return res.status(500).json({
+              error: "Failed to update user",
+            });
+          }
+        }
+        return res.status(400).json({
+          error: info.error.flatten(),
+        });
+      }
+      return res.status(403).json({
+        error: "Only owner allowed to create/edit user",
+      });
+    case "DELETE":
+      if (token.role === Role.enum.owner) {
+        if (token.sub === query.data) {
+          return res.status(400).json({ error: "Can not self-delete account" });
+        }
+
+        try {
+          const deleted = await prisma.user.delete({
+            where: {
+              id: query.data,
+            },
+          });
+          return res
+            .status(200)
+            .json({ message: `User ${deleted.email} deleted` });
+        } catch (e) {
+          if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            if (e.code === "P2003") {
+              return res.status(404).json({
+                error: "User with this id is not exist",
+              });
+            }
+          }
+
+          return res.status(500).json({
+            error: "Failed to delete user",
+          });
+        }
+      }
+      return res.status(403).json({
+        error: "Only owner allowed to delete user",
+      });
+    default:
+      return res.status(405).send({
+        error: "Invalid allowed method",
+      });
+  }
+}
--- pages/api/admin/users/index.ts
+++ pages/api/admin/users/index.ts
@@ -1,8 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Prisma } from "@prisma/client";
 import { prisma } from "@/lib/db";
-import { Role, profileSchema } from "@/lib/schema";
-import { getToken } from "next-auth/jwt";
+import { UserModelSchema } from "@/lib/schema";
 
 export default async function handler(
   req: NextApiRequest,
@@ -11,44 +9,10 @@ export default async function handler(
   switch (req.method) {
     case "GET":
       const users = await prisma.user.findMany();
-      return res.status(200).json({
-        users,
-      });
-    case "POST":
-      const token = await getToken({ req });
-      if (token?.role === Role.enum.owner) {
-        const valid = profileSchema.safeParse(req.body);
-        if (!valid.success) {
-          const error = valid.error.flatten();
-          return res.status(400).json({
-            error: error,
-          });
-        }
-
-        try {
-          await prisma.user.update({
-            where: {
-              email: valid.data.email,
-            },
-            data: {
-              name: valid.data.fullname,
-              role: valid.data.role,
-            },
-          });
-        } catch (e) {
-          if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            if (e.code === "P2001") {
-              return res.status(400).json({
-                error: "Account with this email does not exist",
-              });
-            }
-          }
-        }
-        return res.status(200).json({
-          message: `${valid.data.email} updated successfully`,
-        });
-      }
-      return res.status(403).json({ error: "Only owner can edit accounts" });
+      const response = UserModelSchema.array().safeParse(users);
+      if (response.success)
+        return res.status(200).json({ users: response.data });
+      return res.status(500).json({ error: "Failed to retrive users" });
     default:
       return res.status(405).send({
         error: "Invalid allowed method",
