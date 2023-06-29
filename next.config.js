112b1d6737a91461d02dd76d0a26bc0480ed161a
alkuinvito <alkuinvito@tutanota.com>
2023-06-29 14:09:23 +0700

    fix: migrate to new hostname

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "kasirin.vercel.app",
+        hostname: "kasirin.enigme.dev",
         port: "",
         pathname: "/**",
       },
