8fc9d513fa6a0971151ed097019c6f4deaea28d9
alkuinvito <alkuinvito@tutanota.com>
2023-07-03 11:21:02 +0700

    feat: adjust component props to new sort method

--- components/shared/sortButton.tsx
+++ components/shared/sortButton.tsx
@@ -8,21 +8,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function SortButton({
   title,
   value,
-  state,
+  order,
+  orderBy,
 }: {
   title: string;
   value: string;
-  state: string;
+  order: string;
+  orderBy: string;
 }) {
+  let isActive = value === orderBy;
+  let isDesc = order === "desc";
+
   return (
     <span className="SortTrigger select-none">
       <span>{title}</span>
-      {state !== value && state !== "~" + value ? (
+      {!isActive ? (
         <FontAwesomeIcon
           icon={faSort}
           className="SortIcon hidden ml-2 text-gray-200 dark:text-zinc-700"
         />
-      ) : state === value ? (
+      ) : isDesc ? (
         <FontAwesomeIcon icon={faSortDown} className="ml-2" />
       ) : (
         <FontAwesomeIcon icon={faSortUp} className="ml-2" />
