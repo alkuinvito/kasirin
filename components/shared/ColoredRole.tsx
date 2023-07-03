c89416e7b5dfc923af66f7936f3cec82c6905d62
alkuinvito <alkuinvito@tutanota.com>
2023-07-03 11:20:29 +0700

    feat: change role design

--- components/shared/ColoredRole.tsx
+++ components/shared/ColoredRole.tsx
@@ -4,19 +4,19 @@ export default function ColoredRole({ role }: { role: Role }) {
   switch (role) {
     case Role.Enum.owner:
       return (
-        <span className="px-2 py-1 text-sm font-semibold rounded-lg bg-blue-500 text-white">
+        <span className="px-2 py-1 text-sm font-semibold rounded-full bg-blue-300/50 dark:bg-blue-500/30 text-blue-600 dark:text-blue-400">
           {role}
         </span>
       );
     case Role.Enum.manager:
       return (
-        <span className="px-2 py-1 text-sm font-semibold rounded-lg bg-yellow-500 text-white ">
+        <span className="px-2 py-1 text-sm font-semibold rounded-full bg-yellow-300/50 dark:bg-yellow-500/30 text-yellow-700 dark:text-yellow-400">
           {role}
         </span>
       );
     case Role.Enum.employee:
       return (
-        <span className="px-2 py-1 text-sm font-semibold rounded-lg bg-gray-500 text-white">
+        <span className="px-2 py-1 text-sm font-semibold rounded-full bg-gray-300/50 dark:bg-gray-500/30 text-gray-600 dark:text-gray-400">
           {role}
         </span>
       );
