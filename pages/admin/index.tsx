c205a0c9422e841a3757ecc4eac610dde3f16988
alkuinvito <alkuinvito@tutanota.com>
2023-07-03 11:21:45 +0700

    feat: remove invitation

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,17 +1,15 @@
 import AdminLayout from "@/components/admin/adminLayout";
-import Invitation from "@/components/admin/invitation";
 import UserList from "@/components/admin/userList";
 
 export default function Page() {
   return (
     <AdminLayout>
       <h1 className="font-semibold text-2xl">User Management</h1>
-      <div className="p-4">
+      <div className="mt-4">
         <h2 className="font-semibold text-xl">Members</h2>
-        <div className="mt-4 p-4 max-w-4xl rounded-lg bg-gray-100 dark:bg-zinc-900">
+        <div className="mt-4 max-w-6xl">
           <UserList />
         </div>
-        <Invitation />
       </div>
     </AdminLayout>
   );
