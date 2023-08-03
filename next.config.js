373a53c9f9530ad7348944481740ea9ebdd3b58c
alkuinvito <alkuinvito@tutanota.com>
2023-08-03 16:33:15 +0700

    build: add minio & react-hook-form, add assets url

--- next.config.js
+++ next.config.js
@@ -3,20 +3,8 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/**",
-      },
-      {
         protocol: "https",
-        hostname: "kasirin.enigme.dev",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
+        hostname: "assets.enigme.dev",
         port: "",
         pathname: "/**",
       },
