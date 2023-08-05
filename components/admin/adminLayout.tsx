a47ecd108f55ff44a07c972161993e8a58642546
alkuinvito <alkuinvito@tutanota.com>
2023-08-05 21:19:14 +0700

    feat: add head title to layout

--- components/admin/adminLayout.tsx
+++ components/admin/adminLayout.tsx
@@ -1,9 +1,19 @@
 import { ReactNode } from "react";
+import Head from "next/head";
 import SideBar from "./sidebar";
 
-export default function AdminLayout({ children }: { children: ReactNode }) {
+export default function AdminLayout({
+  children,
+  title,
+}: {
+  children: ReactNode;
+  title?: string;
+}) {
   return (
     <div>
+      <Head>
+        <title>{title && `${title} - `}Kasirin Admin</title>
+      </Head>
       <SideBar />
       <main className="pt-10 ml-72 md:px-6">
         <section className="max-w-6xl mx-auto">{children}</section>
